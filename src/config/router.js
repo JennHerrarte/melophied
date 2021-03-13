c2d983ead1de720d312507b0cbcd300629ed0e3b
Megan Matthews <you@example.com>
2021-03-13 17:54:42 -0600

    created registration page and route

--- src/config/router.js
+++ src/config/router.js
@@ -1,9 +1,11 @@
 import { Switch, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
+import RegistrationPage from '../pages/RegistrationPage/RegistrationPage'
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={ HomePage } />
+        <Route path="/register" component={RegistrationPage} />
     </Switch>
 )
 
