7c90e34e1fdfb8842ca4bafd9ec81ce739a29814
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-13 15:50:20 -0800

    create route for login page

--- src/config/router.js
+++ src/config/router.js
@@ -1,9 +1,11 @@
 import { Switch, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
+import LoginPage from '../pages/LoginPage/LoginPage'
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={ HomePage } />
+        <Route exact path="/login" component={ LoginPage } />
     </Switch>
 )
 
