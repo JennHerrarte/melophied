0cb876a3954831680a4d5fecba710da4684cdaac
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-13 16:42:37 -0800

    boilerplate fix

--- src/config/router.js
+++ src/config/router.js
@@ -1,11 +1,9 @@
 import { Switch, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
-import FanPage from '../pages/FanPage/FanPage'
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={ HomePage } />
-        <Route path="/fanpage" component={ FanPage } />
     </Switch>
 )
 
