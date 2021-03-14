eb76df5da8512fdec4683e439021d70e1003df30
Megan Matthews <you@example.com>
2021-03-14 18:38:11 -0500

    added form for registration - username and password

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,6 +1,40 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form, Button, Container} from 'react-bootstrap';
+import {useState} from 'react';
+
 const RegistrationPage = () => {
+
+    const [username, setUsername]=useState('')
+    const [password, setPassword]=useState('')
+
+    const registrationHandler = () => {
+
+    }
+
     return (
-        <h1>Registration Page</h1>
+        <>
+        <Container>
+        <h1>Register for a Melophied Fan Account</h1>
+
+        <Form onSubmit={registrationHandler}>
+        <Form.Group controlId="username">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Enter Username" name="username" onChange={(e) => {setUsername(e.target.value)}} />
+        <Form.Text className="text-muted">
+        </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" name="password" onChange={(e) => {setPassword(e.target.value)}} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
+        </Form>
+        </Container>
+</> 
     )
 }
 
