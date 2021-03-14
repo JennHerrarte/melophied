acde6f6a109cc83b37a2b6312867ae50e57cd38a
jennuwine <jennherrarte93@gmail.com>
2021-03-14 16:51:38 -0700

    created fannpage bio component

---
+++ src/components/FanPageBio/FanPageBio.jsx
@@ -0,0 +1,9 @@
+const FanPageBio = () => {
+    return(
+        <div className="FanPageBio">
+            <h1>This is the fan page bio</h1>
+        </div>
+    )
+}
+
+export default FanPageBio; 
--- src/pages/FanPage/FanPage.jsx
+++ src/pages/FanPage/FanPage.jsx
@@ -1,7 +1,10 @@
+import FanPageBio from '../../components/FanPageBio/FanPageBio'
+
 const FanPage = () => {
     return (
         <div className="FanPage">
             <h1>FanPage</h1>
+            <FanPageBio/>
         </div>
     )
 }
