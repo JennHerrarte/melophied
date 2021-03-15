96690f08cf0abc9e14374c2aced317d7dbae1120
Megan Matthews <you@example.com>
2021-03-14 19:27:13 -0500

    added first, last name, and email to form and state

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -4,8 +4,11 @@ import {useState} from 'react';
 
 const RegistrationPage = () => {
 
+    const [firstName, setFirstName]=useState('')
+    const [lastName, setLastName]=useState('')
     const [username, setUsername]=useState('')
     const [password, setPassword]=useState('')
+    
 
     const registrationHandler = () => {
 
@@ -15,6 +18,21 @@ const RegistrationPage = () => {
         <>
         <Container>
         <h1>Register for a Melophied Fan Account</h1>
+
+        <Form.Group controlId="confirm-password">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="text" placeholder="First Name" name="firstName" />
+        </Form.Group>
+
+        <Form.Group controlId="confirm-password">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="text" placeholder="Last Name" name="lastName" />
+        </Form.Group>
+
+        <Form.Group controlId="confirm-password">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Email" name="email" />
+        </Form.Group>
 
         <Form onSubmit={registrationHandler}>
         <Form.Group controlId="username">
@@ -27,6 +45,11 @@ const RegistrationPage = () => {
         <Form.Group controlId="password">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" name="password" onChange={(e) => {setPassword(e.target.value)}} />
+        </Form.Group>
+
+        <Form.Group controlId="confirm-password">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" placeholder="Confirm Password" name="confirm-password" />
         </Form.Group>
 
         <Button variant="primary" type="submit">
