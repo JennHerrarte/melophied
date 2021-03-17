d8b3faf26da08f836db4c863dc773eb4ff9312ef
jennuwine <jennherrarte93@gmail.com>
2021-03-16 21:00:26 -0700

    create fan page listings component and added to explore page

---
+++ src/components/FanPageListings/FanPageListings.jsx
@@ -0,0 +1,9 @@
+const FanPageListings = () => {
+    return(
+        <div className='FanPageListings'>
+            <p>Hello I am a listing of fan pages!</p>
+        </div>
+    )
+}
+
+export default FanPageListings;
