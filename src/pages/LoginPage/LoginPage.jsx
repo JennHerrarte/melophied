b8a7ed387876abd6604268456915aca370a5fb75
Megan Matthews <you@example.com>
2021-03-16 22:05:50 -0500

    created LoginForm component, added to LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,10 @@
+import LoginForm from '../../components/LoginForm/LoginForm';
+
 const LoginPage = () => {
     return (
         <div className="LoginPage">
             <h1>Login Page</h1>
+            <LoginForm/>
         </div>
     )
 }
