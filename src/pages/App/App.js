fa2864fadc28d380f6a3c5f00379fa6bf0af9e91
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-17 11:28:36 -0700

    add currenUser state and logout function, passed to Navbar and Routes

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,14 +1,30 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import './App.css';
 import Routes from '../../config/router'
 import Navbar from '../../components/Navbar/Navbar'
 
 function App() {
 
+  const [ currentUser, setCurrentUser ] = useState( localStorage.getItem('uid') );
+  const history = useHistory()
+
+  const logout = () => {
+
+    localStorage.clear()
+
+    setCurrentUser(localStorage.getItem('uid'))
+    history.push('/');
+
+  }
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar currentUser={ currentUser } logout={ logout } />
       <main>
-        <Routes />
+        <Routes currentUser={ currentUser }
+          setCurrentUser={ setCurrentUser } />
       </main>
     </div>
   );
