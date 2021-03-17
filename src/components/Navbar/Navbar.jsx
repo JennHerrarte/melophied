5f9f0c05b69588c20431cb2a219efd60e11100b4
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-17 11:35:38 -0700

    add onclick method for logout and conditional to show login/user dropdown in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,30 +2,34 @@ import {Link} from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 import Dropdown from 'react-bootstrap/Dropdown'
 
-const Navbar = () => {
+const Navbar = ({ currentUser, logout }) => {
     return (
         <nav className="nav d-flex justify-content-between">
             <section className="nav__links">
                 <Link className="nav__link" to="/">Home</Link>
             </section>
             <section className="nav__user">
-                {/* will need a conditional to check if a user is logged in or not -- props.currentUser ? 
-                    if user is logged in, display dropdown. If not, display login button
-                */}
-                <Link to="/login">
-                    <Button>Login</Button>
-                </Link>
-                {/* Will need to adjust logout dropdown option to clear localStorage -- localStorage.clear() */}
-                {/* <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        username
-                    </Dropdown.Toggle>
+                {/* if currentUser is found (localStorage's uid), display user dropdown. If not, show login button */}
+                {
+                    currentUser ? 
+                    (
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                username
+                            </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="/profile">Profile</Dropdown.Item>
-                        <Dropdown.Item href="/logout">Logout</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown> */}
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+                                <Dropdown.Item onClick={ logout }>Logout</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    ) : 
+                    (
+                        <Link to="/login">
+                            <Button>Login</Button>
+                        </Link>
+                    )
+                }
             </section>
         </nav>
     )
