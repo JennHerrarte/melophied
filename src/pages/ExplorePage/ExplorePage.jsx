2852a4780d66a848f7293f46f0d66228b07cf6e1
jennuwine <jennherrarte93@gmail.com>
2021-03-16 20:37:26 -0700

    added explore page aand imported fan pages container

---
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -0,0 +1,12 @@
+import FanPagesContainer from '../../components/FanPagesContainer/FanPagesContainer'
+
+const ExplorePage = () => {
+    return(
+        <div className>
+            <p>I am the explore page so please explore me</p>
+            <FanPagesContainer/>
+        </div>
+    )
+}
+
+export default ExplorePage;
