fcb1c1e852d68aed5e58326d6669114d37da7b2e
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-17 22:55:46 -0700

    add search result div below the input to show search result data

--- src/components/ArtistSelector/ArtistSelector.jsx
+++ src/components/ArtistSelector/ArtistSelector.jsx
@@ -3,20 +3,29 @@ import './ArtistSelector.css'
 const ArtistSelector = ({token, fetchArtists, artists, setArtists, setArtistId }) => {
 
     return (
+
         <div className="ArtistSelector">
-            <input type="text" className="artistSearchBar" placeholder="Find your artist" 
+                {/* when searchbar input changes, fetch Artists. If searchbar is blank, setArtist to empty array */}
+            <input type="text" className="ArtistSelector__artistSearchBar" placeholder="Find your artist" 
             onChange={(e) => e.target.value ? fetchArtists(token, e.target.value): setArtists([])} />
+                {/* if artists is not empty, show search result below the input */}
             {
                 artists.length ? 
-                <div class="searchResult">
+                <div class="ArtistSelector__searchResult">
+                    {/* show clickable paragraph for each artists in the array. When artist is clicked, sets the ID of the artist for the FanPage */}
                     {
-                        artists.map((artist) => <p key={artist.id}>{artist.name}</p> )
+                        artists.map((artist) => <p className="ArtistSelector__artist" key={artist.id} onClick={() => {
+                            setArtistId(artist.id)
+                        }}>{artist.name}</p> )
                     }
+
                 </div> 
                 : 
                 ''
             }
+
         </div>
+
     )
 }
 
