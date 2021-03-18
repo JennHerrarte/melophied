adee78822be5f2bb3d71359f99721afaadc58727
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-17 21:27:12 -0700

    add artist selector component

--- src/components/FanPageEditor/FanPageEditor.jsx
+++ src/components/FanPageEditor/FanPageEditor.jsx
@@ -1,6 +1,7 @@
 const FanPageEditor = () => {
     return(
         <div className="FanPageEditor">
+            Fan Page Editor
             <input type="text" className="pageTitle" placeholder="Page Title"/>
         </div>
     )
--- src/pages/CreateFanPage/CreateFanPage.jsx
+++ src/pages/CreateFanPage/CreateFanPage.jsx
@@ -1,8 +1,12 @@
 import FanPageEditor from '../../components/FanPageEditor/FanPageEditor'
+import ArtistSelector from '../../components/ArtistSelector/ArtistSelector'
 
 const CreateFanPage = () => {
+
+
     return (
         <div className="CreateFanPage">
+            <ArtistSelector />
             <FanPageEditor />
         </div>
     )
