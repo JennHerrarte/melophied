c5f1e422ea037082295d04dfc27d4e1daecde4df
jennuwine <jennherrarte93@gmail.com>
2021-03-17 20:28:26 -0700

    created artist top tracks component

---
+++ src/components/AritistTopTracks/ArtistTopTracks.jsx
@@ -0,0 +1,14 @@
+const ArtistTopTracks = () => {
+    return(
+        <div className='ArtistTopTracks'>
+            <p>I am the artist top tracks!</p>
+            <ul>
+                <li>Coding is my life</li>
+                <li>Coding broke my heart</li>
+                <li>Coding is amazing </li>
+                <li>I can't live without coding</li>
+                <li>Coding is my best friend</li>
+            </ul>
+        </div>
+    )
+}
