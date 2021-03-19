9e35e5688b46fcc296590fbc4bceba85a47d9660
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-19 16:14:35 -0700

    track list editor mounted

--- src/components/FanPageEditor/FanPageEditor.jsx
+++ src/components/FanPageEditor/FanPageEditor.jsx
@@ -1,14 +1,16 @@
 import DetailEditor from '../DetailEditor/DetailEditor'
+import TrackListEditor from '../TrackListEditor/TrackListEditor'
 
 const FanPageEditor = () => {
     return(
         <div className="FanPageEditor">
             Fan Page Editor
             <DetailEditor />
+            <TrackListEditor />
             {/* 
             Checklist:
-            1. Detail Editor
-            2. Track List Editor
+            DONE 1. Detail Editor
+            Mounted 2. Track List Editor
             3. Album List Editor
             4. Show List Editor
             5. Submit button with logic to send POST request to the server
---
+++ src/components/TrackListEditor/TrackListEditor.jsx
@@ -0,0 +1,9 @@
+const TrackListEditor = () => {
+    return (
+        <div className="TrackListEditor d-flex flex-column">
+            TrackList
+        </div>
+    )
+}
+
+export default TrackListEditor
