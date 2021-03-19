c5e783bd45fab3a1724dc204b68308d747daee6d
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-19 16:12:14 -0700

    add page detail field

--- src/components/DetailEditor/DetailEditor.jsx
+++ src/components/DetailEditor/DetailEditor.jsx
@@ -1,7 +1,9 @@
 const DetailEditor = () => {
     return (
-        <div className="DetailEditor">
-            <input type="text" className="pageTitle" placeholder="Page Title"/>
+        <div className="DetailEditor d-flex flex-column">
+            <input type="text" name="pageTitle" className="pageTitle" placeholder="Page Title" maxlength="60"/>
+            <textarea name="pageDetail" placeholder="Page Detail" maxlength="250">
+            </textarea>
         </div>
     )
 }
