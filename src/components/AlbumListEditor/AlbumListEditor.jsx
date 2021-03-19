b06a6a2e14f7504f5817992e7c4d2e60967e8423
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-19 16:16:06 -0700

    album list editor mounted

---
+++ src/components/AlbumListEditor/AlbumListEditor.jsx
@@ -0,0 +1,9 @@
+const AlbumListEditor = () => {
+    return (
+        <div className="AlbumListEditor d-flex flex-column">
+            AlbumList
+        </div>
+    )
+}
+
+export default AlbumListEditor
--- src/components/FanPageEditor/FanPageEditor.jsx
+++ src/components/FanPageEditor/FanPageEditor.jsx
@@ -1,5 +1,6 @@
 import DetailEditor from '../DetailEditor/DetailEditor'
 import TrackListEditor from '../TrackListEditor/TrackListEditor'
+import AlbumListEditor from '../AlbumListEditor/AlbumListEditor'
 
 const FanPageEditor = () => {
     return(
@@ -7,11 +8,12 @@ const FanPageEditor = () => {
             Fan Page Editor
             <DetailEditor />
             <TrackListEditor />
+            <AlbumListEditor />
             {/* 
             Checklist:
             DONE 1. Detail Editor
             Mounted 2. Track List Editor
-            3. Album List Editor
+            Mounted 3. Album List Editor
             4. Show List Editor
             5. Submit button with logic to send POST request to the server
             */}
