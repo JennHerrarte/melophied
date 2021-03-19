59c22934fe244a35c1b72d4022ef76c70ab82849
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-19 16:06:32 -0700

    DetailEditor mounted to FanPageEditor

---
+++ src/components/DetailEditor/DetailEditor.jsx
@@ -0,0 +1,9 @@
+const DetailEditor = () => {
+    return (
+        <div className="DetailEditor">
+            <input type="text" className="pageTitle" placeholder="Page Title"/>
+        </div>
+    )
+}
+
+export default DetailEditor
--- src/components/FanPageEditor/FanPageEditor.jsx
+++ src/components/FanPageEditor/FanPageEditor.jsx
@@ -1,8 +1,18 @@
+import DetailEditor from '../DetailEditor/DetailEditor'
+
 const FanPageEditor = () => {
     return(
         <div className="FanPageEditor">
             Fan Page Editor
-            <input type="text" className="pageTitle" placeholder="Page Title"/>
+            <DetailEditor />
+            {/* 
+            Checklist:
+            1. Detail Editor
+            2. Track List Editor
+            3. Album List Editor
+            4. Show List Editor
+            5. Submit button with logic to send POST request to the server
+            */}
         </div>
     )
 }
