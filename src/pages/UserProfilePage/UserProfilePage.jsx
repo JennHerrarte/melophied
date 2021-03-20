470c12269341b32b45ffbb4ce72ecd5810a7679f
Megan Matthews <you@example.com>
2021-03-20 13:12:16 -0500

    added username to profile page view

--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -1,6 +1,17 @@
-const UserProfilePage = () => {
+import {useState, useEffect} from 'react'
+import jwtDecode from 'jwt-decode'
+
+const UserProfilePage = ({currentUser}) => {
+
+    const [username, setUsername] = useState('')
+
+    useEffect(() => {
+        // decode username and set it as current username
+        if (currentUser) setUsername(jwtDecode(currentUser).username)
+    }, [currentUser])
+
     return (
-        <h1> User Profile Page</h1>
+        <h1>{username}</h1>
     )
 }
 
