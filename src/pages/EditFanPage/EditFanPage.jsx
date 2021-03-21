d68a11d19861be4875867287e8c97ed218a7c7a7
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-21 16:22:23 -0700

    edit routing set

--- src/pages/EditFanPage/EditFanPage.jsx
+++ src/pages/EditFanPage/EditFanPage.jsx
@@ -1,18 +1,25 @@
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 import FanPageEditor from '../../components/FanPageEditor/FanPageEditor'
 import ArtistSelector from '../../components/ArtistSelector/ArtistSelector'
 import Spotify from '../../services/spotify'
+import FanPageAPI from '../../Models/FanPageAPI.js'
 
 const EditFanPage = ({currentUser}) => {
 
     const [token, setToken] = useState('')
     const [artistData, setArtistData] = useState({name: '', id: '', artistImage: ''})
     const [artists, setArtists] = useState([])
+    const [pageData, setPageData] = useState({})
+
+    const params = useParams()
+    const pageId = params.id
 
     // fetch spotify token when the home page loads
     useEffect(() => {
         fetchToken()
+        fetchPageData(pageId)
     }, [])
 
     // feth spotify token and set it to token state
@@ -22,6 +29,14 @@ const EditFanPage = ({currentUser}) => {
 
         setToken(res.data.access_token)
     }
+
+    const fetchPageData = async(pageId) => {
+        const res = await FanPageAPI.show(pageId)
+
+        setPageData(res.data.foundFanPage)
+        setArtistData(res.data.foundFanPage.artistData)
+    }
+
 
     // fetch artist array from spotify and set it to artists state, pass it to ArtistSelector component to display
     const fetchArtists = async (token, artist) => {
