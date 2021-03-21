5806ab2a095d7852ea57b90b2e45bd2c4050c4a7
chuckchoiboi <hyunchoi94@gmail.com>
2021-03-20 17:07:45 -0700

    cleaned some code used for testing

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,36 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import {Button} from 'react-bootstrap';
 import {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
-import Spotify from '../../services/spotify'
 import FanPageCardsContainer from '../../components/FanPageCardsContainer/FanPageCardsContainer'
 import FanPageAPI from '../../Models/FanPageAPI'
 
 const HomePage = ({currentUser}) => {
 
-        const [token, setToken] = useState('')
-        const [artists, setArtists] = useState([])
-        const [topFiveFanPages, setTopFiveFanPages]=useState([])
+    const [topFiveFanPages, setTopFiveFanPages]=useState([])
 
-        // fetch spotify token when the home page loads
-        useEffect(() => {
-            fetchToken()
-        }, [])
-
-        // feth spotify token and set it to token state
-        const fetchToken = async () => {
-
-            const res = await Spotify.getToken()
-
-            setToken(res.data.access_token)
-        }     
-
-        // feth artist array from spotify and set it to artists state
-        const fetchArtists = async (token, artist) => {
-            const res = await Spotify.searchArtists(token, artist)
-
-            setArtists(res.data.artists.items)
-        }
- 
     useEffect(() => {
         
         fetchTopFiveFanPages()
