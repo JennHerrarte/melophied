1d517fd90423f8a48e36fe659117230adaba1915
Megan Matthews <you@example.com>
2021-03-20 19:13:36 -0500

    fetch all pages

--- src/components/FanPageListings/FanPageListings.jsx
+++ src/components/FanPageListings/FanPageListings.jsx
@@ -1,7 +1,10 @@
-const FanPageListings = () => {
+const FanPageListings = ({allFanPages}) => {
+
+    console.log(allFanPages)
     return(
         <div className='FanPageListings'>
             <p>Hello I am a listing of fan pages!</p>
+        
         </div>
     )
 }
