b26ba2259f8771d9a37a1365538ebd931f79b8c7
Megan Matthews <you@example.com>
2021-03-23 00:01:15 -0500

    deleted old code

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -57,65 +57,6 @@ const LoginForm = () => {
     }
   };
   
-  
-
-    {/* <div className="LoginForm">
-      <Container>
-        <h1>Log in to your Melophied Fan Account</h1>
-
-        <Form onSubmit={loginHandler}>
-          <Form.Group controlId="username">
-            <Form.Label>Username or Email</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter Username or email"
-              name="username"
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-              value={username}
-            />
-          </Form.Group>
-
-          <Form.Group controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-          </Form.Group>
-
-          <Form.Group controlId="rememberMe" type="checkbox"
-              name="rememberMe">
-            <Form.Label>Remember Me</Form.Label>
-            <Form.Control
-              type="checkbox"
-              name="rememberMe"
-              value={rememberMe}
-              checked={JSON.parse(rememberMe)}
-              onChange={(e) => {
-                setRememberMe(!rememberMe);
-              }}
-            />
-          </Form.Group>
-
-
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
-        </Form>
-
-        <p>
-          Don't have a Melophied account?
-          <Link to="/register">Register here</Link>
-        </p>
-      </Container>
-    </div> */}
-
     return (
       <>
     <main className="LoginForm">
