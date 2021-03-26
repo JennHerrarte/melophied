ce570ab642cdf6bb998f453e917d50383b4380d2
jennuwine <jennherrarte93@gmail.com>
2021-03-25 22:32:36 -0700

    addeed usrename to user tracks and albums  card

--- src/components/UserTracks/UserTracks.jsx
+++ src/components/UserTracks/UserTracks.jsx
@@ -2,13 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from 'react-bootstrap'
 import './UserTracks.css'
 
-const UserTracks = ({userTracks}) => {
+const UserTracks = ({userTracks, username}) => {
 
     return(
         <Card style={{ width: '25rem', height: '40rem' }}>
         <Card.Body>
           <h1 className='maincardtitle'>Tracks</h1>
-          <Card.Title>My Top 5 Tracks</Card.Title>
+          <Card.Title>{username}'s Top 5 Tracks</Card.Title>
           <Card.Text>
             {
               userTracks ?
